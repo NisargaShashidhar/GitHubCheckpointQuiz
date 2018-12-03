7dd214cf9a7cf81522194a9a5938b79a33fae65e
Nisarga Shashidhar <45395264+NisargaShashidhar@users.noreply.github.com>
2018-12-03 09:53:58 -0500

    Add Test Method to GitTest.java
    This added a method called test to GitTest that will return instance when called upon.

--- GitTest.java
+++ GitTest.java
@@ -1,10 +1,16 @@
 public class GitTest{
 
 	private String instance;
-	
+
 	public GitTest(){
-	
+
 		instance = "This is my GitHub test";
-	
+
+	}
+
+	public String test(){
+
+		return instance;
+
 	}
 }
